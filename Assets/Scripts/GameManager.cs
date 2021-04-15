e4cf8c2ad15e2edcb9d57dac4a83ef9fc74e96a9
Arvind <Arvind@DESKTOP-1H3RAB5>
2021-04-16 00:49:48 +0530

    Added Loding Screen, and Keystore and options changed for android,windows

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+
+namespace Zain360
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameManager : MonoBehaviour
     {
-        
+        public Material domeMaterial;
+
+        public Canvas UICanvas;
+        public Text updateText;
+
+        public void ShowCanvas()
+        {
+            UICanvas.gameObject.SetActive(true);
+        }
+
+        public void HideCanvas()
+        {
+            UICanvas.gameObject.SetActive(false);
+        }
+
+        public void PathPrepared(String path)
+        {
+            print("Path Prepared: " + path);
+            //updateText.text = "Path Prepared";
+        }
+
+        public void Opening()
+        {
+            print("Opening");
+        }
+
+        public void Buffering(float percentage)
+        {
+            print("Buffering: " + percentage);
+            //updateText.text = "Buffering";
+        }
+
+        public void ImageReady(UnityEngine.Object texture)
+        {
+            print("Image Ready");
+        }
+
+        public void Prepared(int width, int height)
+        {
+            print("Prepared: " + width + "x" + height);
+        }
+
+        public void Playing()
+        {
+            print("Playing");
+
+            HideCanvas();
+        }
+
+        public void OnError()
+        {
+            print("SOME ERROR!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
