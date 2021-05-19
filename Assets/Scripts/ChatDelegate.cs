5dfd8067f30d721ef1d0d7acad8b40dd40c88c70
Arvind <Arvind@DESKTOP-1H3RAB5>
2021-05-19 12:16:25 +0530

    Added CD

---
+++ Assets/Scripts/ChatDelegate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Zain360
+{
+    public class ChatDelegate : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
